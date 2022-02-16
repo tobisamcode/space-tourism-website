c8de68ddfb13c1de573199ad4816abe9dbe9a87c
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-02-16 20:01:10 +0100

    new changes

---
+++ starter-code/index.js
@@ -0,0 +1,20 @@
+const nav = document.querySelector(".primary-mavigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+
+
+// when someone clicks the hamburger button 
+navToggle.addEventListener("click", () => {
+
+    const visibility = nav.getAttribute("data-visible")
+        // if menu is closed, open it
+    if (visibility === "false") {
+        nav.setAttribute("data-visible", true)
+    } else {
+        nav.setAttribute("data-visible", false)
+
+    }
+
+
+    // if menu is opened, close it
+})
