8ac52d12d7d6922844069041ca44edc887482c11
tobisamcode <samueloluwatobiloba48@gmail.com>
2022-02-17 23:25:30 +0100

    new changes

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,12 +1,15 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
+
 // when someone clicks the hamburger button 
 navToggle.addEventListener("click", () => {
     const visibility = nav.getAttribute("data-visible");
     if (visibility === "false") {
-        nav.setAttribute("data-visible", true)
+        nav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true);
     } else {
-        nav.setAttribute("data-visible", false)
+        nav.setAttribute("data-visible", false);
+        navToggle.setAttribute("aria-expanded", false);
     }
 })
